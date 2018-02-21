338c85a1f991afdad0ad0ba77db0715fba40ca76
katalinkovacs <katalinkovacsnz@gmail.com>
2018-02-21 14:39:49 +1100

    added more things to customer3 and blueprint.xml Clean install --> SUCCESS

--- TransformationXmlJson/src/main/java/processors/Processor3.java
+++ TransformationXmlJson/src/main/java/processors/Processor3.java
@@ -1,6 +1,26 @@
 package processors;
 
+import customer1.Customer1;
+import customer3.Customer3;
+import org.apache.camel.Exchange;
+
 public class Processor3 {
+
+
+    public void processor3Method (Exchange ex) throws Exception {
+
+        ex.getIn().setHeader("CamelFileName", "Customer1toCustomer3.json");
+
+        Customer1 c1 = (Customer1) ex.getIn().getBody();
+        String firstName = c1.getFirstname();
+        String lastName = c1.getLastname();
+
+        Customer3 c3 = new Customer3();
+        c3.setGivenName(firstName);
+        c3.setFamilyName(lastName);
+
+        ex.getIn().setBody(c3);
+    }
 
 
 }
--- TransformationXmlJson/src/main/java/routes/Route3.java
+++ TransformationXmlJson/src/main/java/routes/Route3.java
@@ -1,4 +1,32 @@
 package routes;
 
-public class Route3 {
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import processors.Processor3;
+
+public class Route3 extends RouteBuilder {
+
+    Processor3 processor3Object = new Processor3();
+
+    public void configure() throws Exception {
+
+        //marshall method needs a java object called jaxbDataFormat1
+        JaxbDataFormat myCustomer1jaxbDataFormat = new JaxbDataFormat("customer1");
+
+        JacksonDataFormat myCustomer3JsonFormat = new JacksonDataFormat(customer3.Customer3.class);
+
+        //from("file:Transformation/src/main/resources/data/inbox/inbox2?noop=true")
+        from("file:C:/Katalin/GitHub/FileReadings/ProjectCamelTransformation/From3")
+        //from("amq:route2.data.in")
+                .id("route3")
+                //unmarshalling is the process to read in xml/json/etc and convert to java object using jaxb generated template
+                .unmarshal(myCustomer1jaxbDataFormat)
+                .bean(processor3Object, "processor3Method")   // object + method
+                //marshalling is the process to convert java to xml/json/etc
+                .marshal(myCustomer3JsonFormat)
+                //.to("file:Transformation/src/main/resources/data/outbox");
+                //.to("amq:route2.data.out");
+                .to("file:C:/Katalin/GitHub/FileReadings/ProjectCamelTransformation/To3");
+    }
 }
